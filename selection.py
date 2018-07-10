66572ee143ec7fe7fde03925ce9941341d2b2c91
Fonsik <fonsik97@gmail.com>
2018-07-10 14:13:04 +0200

    sdg

--- selection.py
+++ selection.py
@@ -15,10 +15,7 @@ chainB.Add("MC_resonance_search_background_small.root/ntuple")
 chainD = TChain("ntuple","")
 chainD.Add("data.root/ntuple")
 
-od=TFile("out_dat2.root", "recreate")
-os=TFile("out_sig2.root", "recreate")
-ob=TFile("out_bkg2.root", "recreate")
-noe=TFile("out_bkg2.txt", "recreate")
+
 
 #chainB.Print()
 #fin = ROOT.TFile.Open("ntuple.root")
@@ -30,58 +27,70 @@ plus = std.vector('int')()
 minus = std.vector('int')()
 minv=ROOT.TH1F("minv", "", 300, 0, 40)
 h_x=TH1F("h_x", "", 100, -1e-4, 1e-4)
-
-for chain in {chainS, chainB, chainD}:
-#    print chain
-
-    entries = chain.GetEntriesFast()
-    noe+=entries    
-
-
-    for jentry in xrange( entries ):
-    # get the next tree in the chain
-      ientry = chain.LoadTree(jentry)
-      if ientry < 0:
-         break
-
-    # verify file/tree/chain integrity
-      nb = chain.GetEntry( jentry )
-      if nb <= 0:
-         continue
-     
-      #find pairs
-      plus.clear()
-      minus.clear()
+lst=[chainS,chainB,chainD]
+lst2=["out_sig2.root","out_bkg2.root","out_dat2.root"]
+file=open("liczba.txt", "w")
+for x in range(3):
+	for chain in (lst[x]):
+	#    print chain
+		od=TFile(lst2[x], "recreate")
+		entries = chain.GetEntriesFast()
+		noe+=entries
+		
+		  
 
 
-      if chain.x.size()>=2:
-	for i in xrange(chain.x.size()):
-         # some cut-like selection
-         if chain.mu_like[i]>0.85:
-          if ( chain == chainS and abs(chain.id[i])==13) or (chain == chainB ):
-           if chain.charge[i]==1:
-            plus.push_back(i)
-	   elif chain.charge[i]==-1:
-            minus.push_back(i)
+		#for jentry in xrange( entries ):
+		# get the next tree in the chain
+		 # ientry = chain.LoadTree(jentry)
+		  #if ientry < 0:
+			# break
 
-     
+		# verify file/tree/chain integrity
+		  #nb = chain.GetEntry( jentry )
+		  #if nb <= 0:
+			# continue
+		 
+		  #find pairs
+		plus.clear()
+		minus.clear()
 
-           
-      for i in xrange(plus.size()):
-          plusV = TLorentzVector(0,0,0,0)
-          plusV.SetPxPyPzE(chain.px[plus[i]],chain.py[plus[i]],chain.pz[plus[i]],chain.e[plus[i]])
-          for j in xrange(minus.size()):
-              minusV = TLorentzVector(0,0,0,0)
-              minusV.SetPxPyPzE(chain.px[minus[j]],chain.py[minus[j]],chain.pz[minus[j]],chain.e[minus[j]])
-              deltaPhi=plusV.DeltaPhi(minusV)
-              deltaZ=chain.z[minus[j]]-chain.z[plus[i]]
-              pT1 = plusV.Pt()
-              pT2 = minusV.Pt()
-              mu_like_1=chain.mu_like[plus[i]]
-              mu_like_2=chain.mu_like[minus[j]]
-              #if abs(deltaPhi)>1 and abs(deltaZ)<0.04
-               #invMass = (plusV+minusV).M()
-               #minv.Fill(invMass)
+
+		if chain.x.size()>=2:
+			for i in xrange(chain.x.size()):
+				if (abs(chain.id[i])==13 or chain.mu_like[i]>0.85):
+					if chain.charge[i]==1:
+						plus.push_back(i)
+					elif chain.charge[i]==-1:
+						minus.push_back(i)
+
+			 
+
+				   
+				for i in xrange(plus.size()):
+					plusV = TLorentzVector(0,0,0,0)
+					plusV.SetPxPyPzE(chain.px[plus[i]],chain.py[plus[i]],chain.pz[plus[i]],chain.e[plus[i]])
+					for j in range(minus.size()):
+						minusV = TLorentzVector(0,0,0,0)
+						minusV.SetPxPyPzE(chain.px[minus[j]],chain.py[minus[j]],chain.pz[minus[j]],chain.e[minus[j]])
+						deltaPhi=plusV.DeltaPhi(minusV)
+						deltaZ=chain.z[minus[j]]-chain.z[plus[i]]
+						#pT1 = plusV.Pt()
+						#pT2 = minusV.Pt()
+						if (abs(deltaPhi)>1 and abs(deltaZ)<0.04):
+							invMass = (plusV+minusV).M()
+							print j
+							print invMass
+							minv.Fill(invMass)
+						   
+	file.write(str(noe))
+	file.write("\n")
+	noe=0				   
+	od.Write()
+	od.Close()
+file.close()
+
+
 
 #TMVA::TMVAGui()
 #ROOT.TMVA.TMVAGui()
