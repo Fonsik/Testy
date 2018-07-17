fce0cdfcc7259128d59a99e4cf5ac4f3efb2acfa
Fonsik <fonsik97@gmail.com>
2018-07-17 09:23:44 +0200

    first

---
+++ ntuple.C
@@ -0,0 +1,182 @@
+#define ntuple_cxx
+#include "ntuple.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <iostream>
+#include <iomanip>
+#include <TChain.h>
+#include <vector>
+#include <TLorentzVector.h>
+
+using namespace std;
+void ntuple::Loop()
+{
+   if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntries();
+
+   TFile output(this->outputFileName.data(), "recreate");
+  // TH1F h_nparticles("h_nparticles", "", 100, 0, 1000);
+  // TH1F h_x("h_x", "", 100, -1e-4, 1e-4);
+  // TH1F h_mu_like("h_mu_like", "", 100, 0, 1);
+   //TH1F h_mu("h_mu", "", 100, 0, 1);
+   //TH1F h_minv("minv", "", 300, 0, 40);
+   TTree *out = new TTree("ntuple","ntuple");
+
+
+    float sa, sb, sab, minv;
+    float angl=0, dis=0;
+    float xp,yp,zp,pxp,pyp,pzp,xm,ym,zm,pxm,pym,pzm,mu_likep,mu_likem, dist;
+
+
+   TLorentzVector xyze;
+   
+   vector <TLorentzVector> vGp, vGpd;
+   vector <TLorentzVector> vGm, vGmd;
+   vector <float> vmu_likem, vmu_likep;
+   
+   
+
+      out->Branch("xp", &xp, "xp/F");
+      out->Branch("yp", &yp, "yp/F");
+	    out->Branch("zp", &zp, "zp/F");
+      out->Branch("xm", &pxp, "zm/F");
+      out->Branch("ym", &pyp, "ym/F");
+      out->Branch("zm", &pzp, "zm/F");
+      out->Branch("pxp", &xm, "pxp/F");
+      out->Branch("pyp", &ym, "pyp/F");
+      out->Branch("pzp", &zm, "pzp/F");
+      out->Branch("pxm", &pxm, "pxm/F");
+      out->Branch("pym", &pym, "pym/F");
+      out->Branch("pzm", &pzm, "pzm/F");
+      out->Branch("dist", &dist, "dist/F");
+      out->Branch("mu_likep", &mu_likep, "mu_likep/F");
+      out->Branch("mu_likem", &mu_likem, "mu_likem/F");
+      
+      out->Branch("minv", &minv, "MINV/F");
+      out->Branch("DeltPhi", &angl, "DP/F");
+      
+    bool isSignal = ( ((std::string)(this->outputFileName.data())).find("sig")  )< 100;
+    cout<<" isSignal = "<<isSignal<<endl;
+    
+
+   int kk=0;
+  
+
+   Long64_t nbytes = 0, nb = 0;
+
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+      int nparticles = id->size();
+     // h_nparticles.Fill(nparticles);
+      for(int particle=0; particle < nparticles; particle++){
+//	h_x.Fill(x->at(particle));
+	//h_x_y.Fill(x->at(particle), y->at(particle));
+	//h_mu_like.Fill(mu_like->at(particle));
+
+
+	if ( mu_like->at(particle)>0.5 &&  ( (isSignal && (id->at(particle)==13||id->at(particle)==-13))   || !isSignal ))
+	 {
+		if(charge->at(particle)>0)
+		{
+		xyze.SetPxPyPzE(px->at(particle),py->at(particle),pz->at(particle),e->at(particle)); vGp.push_back(xyze);
+		xyze.SetXYZM(x->at(particle),y->at(particle),z->at(particle),e->at(particle)); vGpd.push_back(xyze);
+		vmu_likep.push_back(mu_like->at(particle));
+		}
+
+		if(charge->at(particle)<0)
+		{
+		xyze.SetPxPyPzE(px->at(particle),py->at(particle),pz->at(particle),e->at(particle)); vGm.push_back(xyze);
+		xyze.SetXYZM(x->at(particle),y->at(particle),z->at(particle),e->at(particle)); vGmd.push_back(xyze);
+		vmu_likem.push_back(mu_like->at(particle));
+		}
+	}
+
+
+
+
+                                                }
+
+        int proc=100.0*(jentry+1)/nentries;
+        cout<<"Wejscie: "<<jentry+1<<". Wykonano: "<<proc<<"%."<< "\r"<<flush;
+
+//cout<<endl;
+//cout<<vGp.size()<<endl;
+     for (int i=0; i<vGp.size(); i++)
+   {
+        for (int j=0; j<vGm.size(); j++)
+        {    
+		    minv=(vGp[i]+vGm[j]).M();
+	        angl=vGp[i].DeltaPhi(vGm[j]);
+	        xp=vGpd[i][0];
+	        yp=vGpd[i][1];
+	        zp=vGpd[i][2];
+	        pxp=vGp[i][0];
+	        pyp=vGp[i][1];
+	        pzp=vGp[i][2];
+	        xm=vGmd[j][0];
+	        ym=vGmd[j][1];
+	        zm=vGmd[j][2];
+	        pxm=vGm[j][0];
+	        pym=vGm[j][1];
+	        pzm=vGm[j][2];
+          dist=abs(zp-zm);
+	        mu_likep=vmu_likep[i];
+	        mu_likem=vmu_likem[j];
+	        
+	        out->Fill();
+	        
+
+ 
+
+
+     
+
+
+        }
+
+   }
+
+
+   
+   vGp.resize(0);
+   vGm.resize(0);
+   vGpd.resize(0);
+   vGmd.resize(0);
+   vmu_likep.resize(0);
+   vmu_likem.resize(0);
+   }
+
+   out->Write();
+
+   output.Write();
+   output.Close();
+}
+
+int main(int argc, char** argv){
+
+  if(argc < 3){
+    std::cout << "Usage:\n\t" << argv[0] << " output.root input1.root [input2.root input3.root ...]\n\n";
+    return 1;
+  }
+
+  std::cout << "Output: " << argv[1] << "\n";
+  // TChain is like a TTree, but can work across several root files
+  TChain * chain = new TChain("ntuple");
+  std::cout << "Inputs:\n";
+  for(int i=2; i<argc; i++){
+    std::cout << "\t" << argv[i] << "\n";
+    chain->Add(argv[i]);
+  }
+
+  ntuple t(chain);
+  t.outputFileName = argv[1];
+  t.Loop();
+
+  std::cout << "[ DONE ]\n\n";
+
+}
---
+++ plot.C
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+void plot(){
+
+  // opening files
+  //TFile f1("out_dat2.root");
+  //TFile f2("out_sig2.root");
+  //TFile f3("out_bkg2.root");
+  TFile ff("TMVApp.root");
+
+
+  // get histograms from the files
+  //TH1F * h_n_1 = (TH1F*)f1.Get("minv");
+  //TH1F * h_n_2 = (TH1F*)f2.Get("minv");
+  //TH1F * h_n_3 = (TH1F*)f3.Get("minv");
+
+  //TH1F * BDT1 = (TH1F*)ff.Get("minvBDT1");
+ // TH1F * BDT2 = (TH1F*)ff.Get("minvBDT2");
+  TH1F * BDT3 = (TH1F*)ff.Get("minvBDT3");
+
+ /* TH1F * Like1 = (TH1F*)ff.Get("minvLike1");
+  TH1F * Like2 = (TH1F*)ff.Get("minvLike2");
+  TH1F * Like3 = (TH1F*)ff.Get("minvLike3");
+  TH1F * LikeD = (TH1F*)ff.Get("minvD");
+  TH1F * LikeP = (TH1F*)ff.Get("minvPCA");
+
+  TH1F * MLP1 = (TH1F*)ff.Get("minvMLP1");
+  TH1F * MLP2 = (TH1F*)ff.Get("minvMLP2");
+  TH1F * MLP3 = (TH1F*)ff.Get("minvMLP3");*/
+
+  // set line colors
+
+
+  TCanvas c;
+  c.SaveAs("plots2.pdf["); // opening pdf
+
+  // draw all histograms
+
+	//BDT1->SetLineColor(1);
+	//BDT2->SetLineColor(2);
+	BDT3->SetLineColor(3);
+  BDT3->Rebin(20);
+  BDT3->Draw("hist");
+	
+ /* if (BDT1->GetEntries()>0 && BDT2->GetEntries()>0 && BDT3->GetEntries()>0) 
+
+  {
+  BDT1->Rebin(20);
+  BDT2->Rebin(20);
+  
+  BDT1->Draw("hist");
+	BDT2->Draw("hist same");
+  
+  }
+
+	c.SaveAs("plots2.pdf");
+
+  c.Clear();
+
+  Like1->SetLineColor(1);
+  Like2->SetLineColor(2);
+  Like3->SetLineColor(3);
+
+  if (true//Like1->GetEntries()>0 && Like2->GetEntries()>0 && Like3->GetEntries()>0)
+  ){
+  Like1->Rebin(20);
+  Like2->Rebin(20);
+  Like3->Rebin(20);
+  Like1->Draw("hist");
+  Like2->Draw("hist same");
+  Like3->Draw("hist same");
+  }
+
+  c.SaveAs("plots2.pdf");
+
+ 
+  c.Clear();
+
+  MLP1->SetLineColor(1);
+  MLP2->SetLineColor(2);
+  MLP3->SetLineColor(3);
+
+  
+  if (true//MLP1->GetEntries()>0 && MLP2->GetEntries()>0 && MLP3->GetEntries()>0)
+  ){
+  MLP1->Rebin(20);
+  MLP2->Rebin(20);
+  MLP3->Rebin(20);
+  MLP1->Draw("hist");
+  MLP2->Draw("hist same");
+  MLP3->Draw("hist same");
+  }
+
+  c.SaveAs("plots2.pdf");
+ c.Clear();
+
+  BDT3->Draw("hist");
+  c.SaveAs("plots2.pdf");
+   c.Clear();
+
+  LikeD->Rebin(20);
+  LikeP->Rebin(20);
+
+
+  LikeD->Draw("hist");
+  c.SaveAs("plots2.pdf");
+   c.Clear();
+
+  LikeP->Draw("hist");
+*/
+  c.SaveAs("plots2.pdf");
+
+  c.SaveAs("plots2.pdf]"); // closing pdf
+
+
+}
