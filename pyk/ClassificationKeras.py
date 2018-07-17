3ddb84a25f2bce7fef1c41c11aa4e4c21cc58af0
Fonsik <30571284+Fonsik@users.noreply.github.com>
2018-07-17 10:50:34 +0200

    ddd

--- pyk/ClassificationKeras.py
+++ pyk/ClassificationKeras.py
@@ -21,14 +21,32 @@ factory = TMVA.Factory('TMVAClassification', output,
 if not isfile('tmva_class_example.root'):
     call(['curl', '-O', 'http://root.cern.ch/files/tmva_class_example.root'])
 
-dataS = TFile.Open('~/Test/Testy/out_sigk.root')
-dataB = TFile.Open('~/Test/Testy/out_bkgk.root')
+dataS = TFile.Open('~/Test/Testy/out_sig.root')
+dataB = TFile.Open('~/Test/Testy/out_bkg.root')
 signal = dataS.Get('ntuple')
 background = dataB.Get('ntuple')
 
 dataloader = TMVA.DataLoader('dataset')
-for branch in signal.GetListOfBranches():
-    dataloader.AddVariable(branch.GetName())
+#for branch in signal.GetListOfBranches():
+ #   dataloader.AddVariable(branch.GetName())
+
+'''dataloader.AddVariable( "yp")
+dataloader.AddVariable( "zp")
+dataloader.AddVariable( "pxp")
+dataloader.AddVariable( "pyp")
+dataloader.AddVariable( "pzp")
+dataloader.AddVariable( "xm")
+dataloader.AddVariable( "ym")
+dataloader.AddVariable( "zm" )
+dataloader.AddVariable( "pxm")
+dataloader.AddVariable( "pym")
+dataloader.AddVariable( "pzm")'''
+dataloader.AddVariable( "dist")
+dataloader.AddVariable( "DeltPhi")
+dataloader.AddVariable( "mu_likep")
+dataloader.AddVariable( "mu_likem")
+dataloader.AddSpectator( "minv" )
+
 
 dataloader.AddSignalTree(signal, 1.0)
 dataloader.AddBackgroundTree(background, 1.0)
@@ -39,6 +57,7 @@ dataloader.PrepareTrainingAndTestTree(TCut(''),
 
 # Define model
 model = Sequential()
+model.add(Dense(64, activation='relu', W_regularizer=l2(1e-5), input_dim=4))
 model.add(Dense(64, activation='relu', W_regularizer=l2(1e-5), input_dim=4))
 model.add(Dense(2, activation='softmax'))
 
@@ -51,10 +70,12 @@ model.save('model.h5')
 model.summary()
 
 # Book methods
-factory.BookMethod(dataloader, TMVA.Types.kFisher, 'Fisher',
-                   '!H:!V:Fisher:VarTransform=D,G')
+#factory.BookMethod(dataloader, TMVA.Types.kFisher, 'Fisher',
+#                   '!H:!V:Fisher:VarTransform=D,G')
 factory.BookMethod(dataloader, TMVA.Types.kPyKeras, 'PyKeras',
-                   'H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=20:BatchSize=32')
+                   'H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=40:BatchSize=32')
+factory.BookMethod( dataloader,  TMVA.Types.kBDT, "BDT",
+                           "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" )
 
 # Run training, test and evaluation
 factory.TrainAllMethods()
